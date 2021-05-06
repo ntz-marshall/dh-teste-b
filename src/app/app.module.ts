5ffdb079da16f195f508faeca7f13de9418686dd
Marcelle Nhemetz <58017332+ntz-marshall@users.noreply.github.com>
2021-05-06 14:42:27 -0300

    Adicionando um pouco de cor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AnimatedBackgroundComponent } from './animated-background/animated-background.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnimatedBackgroundComponent
   ],
   imports: [
     BrowserModule
